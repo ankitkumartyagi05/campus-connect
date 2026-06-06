9d2603e8cff0b86f27e98079c8c401f082e36df4
Ankit Kumar Tyagi <ankitkumartyagi149@gmail.com>
2026-06-06 10:07:12 +0530

    Delete backend directory

--- backend/app/presentation/frontend_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import FileResponse
-
-router = APIRouter()
-
-@router.get("/")
-async def serve_frontend():
-    return FileResponse("index.html")
